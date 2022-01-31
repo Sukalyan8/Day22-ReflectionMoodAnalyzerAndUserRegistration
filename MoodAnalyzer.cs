786bad4fca6452306b873da950598f1c18f87560
Sukalyan <sukalyandash1920@gmail.com>
2022-01-31 22:02:45 +0530

    MoodAnalyzer_UsingReflection

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer_Reflection
+{
+    public class MoodAnalyzer
+    {
+        public string message;
+        //public MoodAnalyser()
+        //{
+        //}
+
+        public void MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            string mood;
+            if (message == "I am in a Happy mood")
+                mood = "Happy";
+            else if (message == "I am in a Sad mood")
+                mood = "Sad";
+            else
+                mood = null;
+            return mood;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzer_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser App!");
+        }
+    }
+}
